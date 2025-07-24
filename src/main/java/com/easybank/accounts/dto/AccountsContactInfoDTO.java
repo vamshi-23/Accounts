6b3731ad59be4c3658a8ecda4bffc0326e98e97a
vamshi krishna <vamshikrishna1251@outlook.com>
2025-07-24 13:23:40 +0530

    Section -6 changes with docker, config server, rabbit mq, actuator.

--- src/main/java/com/easybank/accounts/dto/AccountsContactInfoDTO.java
+++ src/main/java/com/easybank/accounts/dto/AccountsContactInfoDTO.java
@@ -1,5 +1,7 @@
 package com.easybank.accounts.dto;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
@@ -7,6 +9,12 @@ import java.util.List;
 import java.util.Map;
 
 @ConfigurationProperties(prefix = "accounts")
-public record AccountsContactInfoDTO(String message, Map<String, String> contactDetails, List<String> onCallSupport) {
+@Getter
+@Setter
+public class AccountsContactInfoDTO{
+    private String message;
+    private Map<String, String> contactDetails;
+    private List<String> onCallSupport;
+
 
 }
